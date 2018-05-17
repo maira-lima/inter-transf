517ddcff5df61254af7f0c150d656151ba77392e
Maira Lima <maira.lima@aluno.ufabc.edu.br>
2018-05-17 22:32:38 +0000

    add tests code

--- inter_transf.py
+++ inter_transf.py
@@ -2,28 +2,50 @@
 # inter_transf.py
 # Author/Professor: Fabrício Olivetti de França
 # Author/Student: Maira Zabuscha de Lima
+# # Rede Neural de Múltiplas Camadas para Regressão Simbólica
 
 import numpy as np
-import sys
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LassoCV
-from sklearn.linear_model import LassoLarsCV
+from sklearn.linear_model import LassoCV, LassoLarsCV
+import warnings
+import sys
+import os
+import pickle
 
 # ignoramos os resultados NaN das funções pois vamnos zera-los
 np.seterr(invalid='ignore')
+# nao quero warning de convergência
+warnings.filterwarnings('ignore')
+
+def getResults(fname='results.pkl'):
+    dataset = []
+    algoritmo = []
+    ninter_l = []
+    inter_min_l  = []
+    inter_max_l  = []
+    msre_l = []
+    if os.path.exists(fname):
+        fw = open(fname, 'rb')
+        dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l  = pickle.load(fw)
+        fw.close()
+    return dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l
+
+def storeResults(dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l, fname='results.pkl'):
+    fw = open(fname, 'wb')
+    pickle.dump((dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l), fw)
+    fw.close()
+
+def msre(y_true, y_pred):
+    return np.sqrt(mean_squared_error(y_true, y_pred))
 
 def importaDados(fname):
-    # carrega os dados do arquivo fname e retorna X, y
     dataset = np.loadtxt(fname, delimiter=",")
     X = dataset[:, :-1]
     y = dataset[:, -1]
     return (X, y)
 
 def transformData(X, rede):
-    # recebe uma matriz X e uma rede de expoentes
-    # retorna os dados transformados
-    # substitui todos os valores NaN e Inf para 0
     n_rows = X.shape[0]
     n_inter = rede.shape[1]
     layers = np.ndarray((n_rows, 3*n_inter))
@@ -34,53 +56,65 @@ def transformData(X, rede):
         layers[:,i+2] = np.sqrt(layers[:,i]) # sqrt
     cols = np.any(np.isnan(layers), axis=0)
     layers[:, cols] = 0
+    cols = np.any(np.isinf(layers), axis=0)
+    layers[:, cols] = 0
     return layers
 
-def fit(X, y, n_inter):
-    # Cria a camada de expoentes da rede com n_inter neurônios
-    # Aplica a função transformData em X_train utilizando essa rede
-    # Divida a base entre treino e validação
-    # Aplique o LassoCV e LassoLarsCV, verifique na validação o que retorna o menor erro
-    # Retorne a rede e o modelo de menor erro
-    # expoentes aleatorios de 0 a 2
+def fit(X, y, n_inter, inter_min=0, inter_max=3):
     n_inputs = X.shape[1]
-    exponents = np.random.randint(0,3,size=(n_inputs, n_inter))
+    exponents = np.random.randint(inter_min, inter_max, size=(n_inputs, n_inter))
     X_transf = transformData(X, exponents)
-    X_train, X_test, y_train, y_test = train_test_split(X_transf, y, test_size=0.3, random_state=1)
-    models = {'lassoCV':LassoCV(max_iter=5e4, cv=3), 'lassoLarsCV':LassoLarsCV(max_iter=5e4, cv=3)}
-    rmse = {}
-    for key, model in models.items():
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        rmse[key] = np.sqrt(mean_squared_error(y_test, y_pred))
-        print(key, rmse[key]) # so pra conferir se eu to fazdo certo
-    print('min', min(rmse, key=rmse.get)) # so pra conferir se eu to fazdo certo
-    return exponents, models[min(rmse, key=rmse.get)]
+    lasso = LassoCV(max_iter=5e4, cv=3)
+    lassoLars = LassoLarsCV(max_iter=5e4, cv=3)
+    lasso.fit(X_transf, y)
+    lassoLars.fit(X_transf, y)
+    return exponents, lasso, lassoLars
 
-def predict(X_test, rede, modelo):
-    # Aplica transformData em X_test usando a rede
-    # Aplique o método predict de modelo na base transformada e armazena a saída em y_hat
-    # Retorna y_hat
-    X_transf = transformData(X_test, rede)
+def predict(X, rede, modelo):
+    X_transf = transformData(X, rede)
     y_hat = modelo.predict(X_transf)
     return y_hat
 
-def main(file='airfoil'):
-    NINTER = 5
-    print('n inter', NINTER)
-
-    fileTrain = 'datasets/' + file + '-train-0.dat'
-    X_train, y_train = importaDados(fileTrain)
-    rede, modelo = fit(X_train, y_train, NINTER)
-
-    fileTest = 'datasets/' + file + '-test-0.dat'
-    X_test, y_test = importaDados(fileTest)
-    y_pred = predict(X_test, rede, modelo)
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    print('rmse', rmse)
-
+def main(D, ninter_str):
+    dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l = getResults('results.pkl')
+    #ninter_list = [10, 50, 100, 500, 1000]
+    inter_min_max = [(-1,1), (-2,2), (-3,3), (-4,4), (-5,5), (0,1), (0,2), (0,3), (0,4), (0,5)]
+    pastas = ['0', '1', '2', '3', '4']
+    #bases = ['airfoil', 'concrete', 'cpu', 'energyCooling', 'energyHeating', 'forestfires', 'towerData', 'wineRed', 'wineWhite', 'yacht']
+    #basesquebugam = ['bioavailability', 'ppb']
+    
+    #for D in bases:
+    #for ninter in ninter_list:
+    ninter = int(ninter_str)
+    for (inter_min, inter_max) in inter_min_max:
+        dataset += [D, D]
+        ninter_l += [ninter, ninter]
+        inter_min_l += [inter_min, inter_min]
+        inter_max_l += [inter_max, inter_max]
+        algoritmo += ['lasso', 'lassoLars']
+        msre_lasso = 0
+        msre_lassoLars = 0
+        for pasta in pastas:
+            fileTrain = 'datasets/' + D + '-train-' + pasta + '.dat'
+            X, y = importaDados(fileTrain)
+            n = X.shape[1]
+            X_treino, X_validacao, y_treino, y_validacao = train_test_split(X, y, test_size=0.3, random_state=1)
+            rede, lasso, lassoLars = fit(X_treino, y_treino, ninter*n, inter_min, inter_max)
+            y_lasso = predict(X_validacao, rede, lasso)
+            y_lars = predict(X_validacao, rede, lassoLars)
+            msre_lasso += msre(y_validacao, y_lasso)
+            msre_lassoLars += msre(y_validacao, y_lars)
+            print( D, 'ninter', ninter, 'min_max', inter_min, inter_max, 'pasta', pasta)
+        m = len(pastas)
+        msre_l += [msre_lasso/m, msre_lassoLars/m]
+    
+    storeResults(dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l, 'results.pkl')
+    print('done')
+    
 if __name__ == "__main__":
-    file = 'airfoil'
-    if len(sys.argv) > 1:
-        file = sys.argv[1]
-    main(file)
+    if len(sys.argv) > 2:
+        base = sys.argv[1]
+        ninter = sys.argv[2]
+        main(base, ninter)
+    else:
+        print('determine a base e ninter (nessa ordem)')
