f2acb3aaf2578f98d454556ab1c6b84f7994c667
Maira Lima <maira.lima@aluno.ufabc.edu.br>
2018-05-18 18:43:57 +0000

    implementei a função geraRede

--- inter_transf.py
+++ inter_transf.py
@@ -44,6 +44,14 @@ def importaDados(fname):
     X = dataset[:, :-1]
     y = dataset[:, -1]
     return (X, y)
+    
+def geraRede(X, n_inter, inter_min=0, inter_max=3):
+    n_inputs = X.shape[1]
+    rede = np.random.randint(inter_min, inter_max, size=(n_inputs, n_inter))
+    for i in range(0, n_inputs):
+        if 0 in X[:,i]:
+            rede[i,:] = np.absolute(rede[i,:])
+    return rede
 
 def transformData(X, rede):
     n_rows = X.shape[0]
@@ -62,59 +70,57 @@ def transformData(X, rede):
 
 def fit(X, y, n_inter, inter_min=0, inter_max=3):
     n_inputs = X.shape[1]
-    exponents = np.random.randint(inter_min, inter_max, size=(n_inputs, n_inter))
-    X_transf = transformData(X, exponents)
+    rede = geraRede(X, n_inter, inter_min, inter_max)
+    X_transf = transformData(X, rede)
     lasso = LassoCV(max_iter=5e4, cv=3)
     lassoLars = LassoLarsCV(max_iter=5e4, cv=3)
     lasso.fit(X_transf, y)
     lassoLars.fit(X_transf, y)
-    return exponents, lasso, lassoLars
+    return rede, lasso, lassoLars
 
 def predict(X, rede, modelo):
     X_transf = transformData(X, rede)
     y_hat = modelo.predict(X_transf)
     return y_hat
 
-def main(D, ninter_str):
+def main(D):
     dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l = getResults('results.pkl')
-    #ninter_list = [10, 50, 100, 500, 1000]
+    ninter_list = [10, 50, 100]
     inter_min_max = [(-1,1), (-2,2), (-3,3), (-4,4), (-5,5), (0,1), (0,2), (0,3), (0,4), (0,5)]
     pastas = ['0', '1', '2', '3', '4']
     #bases = ['airfoil', 'concrete', 'cpu', 'energyCooling', 'energyHeating', 'forestfires', 'towerData', 'wineRed', 'wineWhite', 'yacht']
     #basesquebugam = ['bioavailability', 'ppb']
     
     #for D in bases:
-    #for ninter in ninter_list:
-    ninter = int(ninter_str)
-    for (inter_min, inter_max) in inter_min_max:
-        dataset += [D, D]
-        ninter_l += [ninter, ninter]
-        inter_min_l += [inter_min, inter_min]
-        inter_max_l += [inter_max, inter_max]
-        algoritmo += ['lasso', 'lassoLars']
-        msre_lasso = 0
-        msre_lassoLars = 0
-        for pasta in pastas:
-            fileTrain = 'datasets/' + D + '-train-' + pasta + '.dat'
-            X, y = importaDados(fileTrain)
-            n = X.shape[1]
-            X_treino, X_validacao, y_treino, y_validacao = train_test_split(X, y, test_size=0.3, random_state=1)
-            rede, lasso, lassoLars = fit(X_treino, y_treino, ninter*n, inter_min, inter_max)
-            y_lasso = predict(X_validacao, rede, lasso)
-            y_lars = predict(X_validacao, rede, lassoLars)
-            msre_lasso += msre(y_validacao, y_lasso)
-            msre_lassoLars += msre(y_validacao, y_lars)
-            print( D, 'ninter', ninter, 'min_max', inter_min, inter_max, 'pasta', pasta)
-        m = len(pastas)
-        msre_l += [msre_lasso/m, msre_lassoLars/m]
+    for ninter in ninter_list:
+        for (inter_min, inter_max) in inter_min_max:
+            dataset += [D, D]
+            ninter_l += [ninter, ninter]
+            inter_min_l += [inter_min, inter_min]
+            inter_max_l += [inter_max, inter_max]
+            algoritmo += ['lasso', 'lassoLars']
+            msre_lasso = 0
+            msre_lassoLars = 0
+            for pasta in pastas:
+                fileTrain = 'datasets/' + D + '-train-' + pasta + '.dat'
+                X, y = importaDados(fileTrain)
+                n = X.shape[1]
+                X_treino, X_validacao, y_treino, y_validacao = train_test_split(X, y, test_size=0.3, random_state=1)
+                rede, lasso, lassoLars = fit(X_treino, y_treino, ninter*n, inter_min, inter_max+1)
+                y_lasso = predict(X_validacao, rede, lasso)
+                y_lars = predict(X_validacao, rede, lassoLars)
+                msre_lasso += msre(y_validacao, y_lasso)
+                msre_lassoLars += msre(y_validacao, y_lars)
+                print( D, 'ninter', ninter, 'min_max', inter_min, inter_max, 'pasta', pasta)
+            m = len(pastas)
+            msre_l += [msre_lasso/m, msre_lassoLars/m]
     
     storeResults(dataset, algoritmo, ninter_l, inter_min_l, inter_max_l, msre_l, 'results.pkl')
     print('done')
     
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 1:
         base = sys.argv[1]
-        ninter = sys.argv[2]
-        main(base, ninter)
+        main(base)
     else:
-        print('determine a base e ninter (nessa ordem)')
+        print('determine a base')
